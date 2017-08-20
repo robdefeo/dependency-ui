b6dbd820ed3620a9aecec0b17517b2696f607c92
Rob De Feo <robertodefeo@hotmail.com>
2017-08-20 16:47:22 +0100

    create auth guard

---
+++ src/app/core/guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+        if (localStorage.getItem('currentUser')) {
+            // logged in to return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    }
+
+}
