9f325e756f9244aff3f51cf5cf766853729e1155
Rob De Feo <robertodefeo@hotmail.com>
2017-08-20 17:26:15 +0100

    create public index

---
+++ src/app/public/index.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './index.component.html',
+})
+export class PublicIndexComponent {
+    
+}
